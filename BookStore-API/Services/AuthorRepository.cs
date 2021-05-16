c3a26e93da93967958050df8f96ec8a819c40756
ricafort <ricafort@gmail.com>
2021-05-17 08:54:41 +1000

    repository partial

---
+++ BookStore-API/Services/AuthorRepository.cs
@@ -0,0 +1,42 @@
+﻿using BookStore_API.Contracts;
+using BookStore_API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public Task<bool> Create(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Author>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
